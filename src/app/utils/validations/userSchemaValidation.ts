b18db793114653add15c9a98d16480b3903bb90f
Gabriel <ghaabriieel@gmail.com>
2025-09-02 20:37:09 -0300

    adicionando arquivo .env e omitindo info

---
+++ src/app/utils/validations/userSchemaValidation.ts
@@ -0,0 +1,35 @@
+import * as yup from 'yup';
+
+let UserSchema = yup.object().shape({
+    id: yup.number(),
+    name: yup
+    .string()
+    .required("O campo nome é obrigatório")
+    .min(3,"O campo nome deve ter pelo menos 3 caracteres" )
+    .max(100, "O campo nome não pode ter mais de 100 caracteres")
+    ,
+
+    email: yup
+    .string()
+    .email("O campo email deve ser um email válido")
+    .required("O campo email é obrigatório")
+    .min(10, "O campo email deve ter pelo menos 10 caracteres")
+    .max(100, "O campo email não pode ter mais de 100 caracteres"),
+    
+
+    password: yup
+    .string()
+    .required()
+    .max(100, "O campo senha não pode ter mais de 100 caracteres"),
+
+
+    birth_date: yup
+    .string()
+    .typeError("O campo data de nascimento deve ser uma data") // equivale ao "date.base" do Joi
+    .required("O campo data de nascimento é obrigatório"),
+
+    active: yup
+    .boolean()
+    .typeError("O campo ativo deve ser um valor booleano") // equivale ao "boolean.base" do Joi
+    .required("O campo ativo é obrigatório"),
+})
--- src/database/dataSource.ts
+++ src/database/dataSource.ts
@@ -1,15 +1,16 @@
 import "reflect-metadata"
+import "dotenv/config";
 import { DataSource } from "typeorm"
 import {CreateUserTable1756211583661} from "./migrations/1756211583661-CreateUserTable"
 import {CreateSeedUsersTable1756317660505} from "./migrations/1756317660505-createSeedUsersTable"
 import User from "../app/entities/User"
 export const AppDataSource = new DataSource({
     type: "mysql",
-    host: "localhost",
-    port: 3306,
-    username: "root",
-    password: "@Gs189970",
-    database: "curso_typeorm",
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT) || 3306,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
     synchronize: true,
     logging: false,
     entities: [User],
